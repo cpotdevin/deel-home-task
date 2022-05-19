65a3527790aa7eea255b4e7d8e2fdd93ce41928f
Christian Potdevin <chrpotdevin@gmail.com>
2022-05-18 21:35:17 -0500

    fix: return contract only if it belongs to the profile calling

--- src/app.js
+++ src/app.js
@@ -8,15 +8,22 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
-/**
- * FIX ME!
- * @returns contract by id
- */
 app.get('/contracts/:id', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models');
   const { id } = req.params;
   const contract = await Contract.findOne({ where: { id } });
-  if (!contract) return res.status(404).end();
+
+  if (!contract) {
+    return res.status(404).end();
+  }
+
+  if (
+    contract.ContractorId !== req.profile.id &&
+    contract.ClientId !== req.profile.id
+  ) {
+    return res.status(403).end();
+  }
+
   res.json(contract);
 
   return null;
